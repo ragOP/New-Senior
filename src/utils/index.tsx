8c371234e8391f0df5c3fc4e2e9ba7a6c52675dc
farish-jamal <farishjamal98@gmail.com>
2024-07-18 21:03:17 +0530

    adedd

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,7 +2,7 @@ import { animateScroll } from "../components/animationScroll";
 
 const getElementPosition = (element: any) => element.offsetTop;
 
-export const scrollTo = ({ id, ref = null, duration = 1500 }: any) => {
+export const scrollTo = ({ id, ref = null, duration = 1500, offset = 20 }: any) => {
     // the position of the scroll bar before the user clicks the button
     const initialPosition = window.scrollY;
 
@@ -12,12 +12,15 @@ export const scrollTo = ({ id, ref = null, duration = 1500 }: any) => {
 
         if (!element) {
             // log error if the reference passed is invalid
+            console.error('Element not found');
             return;
         }
 
+        // target position is the element's offsetTop minus the offset
+        const targetPosition = getElementPosition(element) - offset;
+
         animateScroll({
-            // target position is the elements offsetTop
-            targetPosition: getElementPosition(element),
+            targetPosition,
             initialPosition,
             duration
         });
